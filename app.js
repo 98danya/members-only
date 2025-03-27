67690ed78b890295b1d10a5fe95e1ae01264d6d5
98.danya <1998.danyamohammed@gmail.com>
2025-03-27 02:19:13 +0100

    Added a simple layout

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
